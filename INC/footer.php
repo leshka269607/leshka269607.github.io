5a016723e1bad4117d01d704b6b6261a52e39065
leshka269607 <leshka.waw@mail.ru>
2016-01-17 12:28:26 +0300

    Nerds

--- INC/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer">
-  <div class="container clearfix">
-    <p class="id-info" title="Количество обработанных строк за всё время!"><?=file_get_contents('id.txt');?></p>
-  </div>
-</footer>
